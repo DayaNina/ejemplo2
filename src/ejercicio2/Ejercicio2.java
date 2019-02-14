253da0a187e5e0988e4a2eaaab4042e3e04f916c
Dayana Nina <dayananinan2@gmail.com>
2019-02-14 08:39:47 -0600

    Hola

---
+++ src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio2;
+
+/**
+ *
+ * @author Dayana
+ */
+public class Ejercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("hola mundo");
+        // TODO code application logic here
+    }
+    
+}
